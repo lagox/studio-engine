93e8f455f3ed479a691395b33d01201ccedc6dd6
lagox <lagox.mail@gmail.com>
2011-03-09 00:38:56 +0600

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Studio::Application.config.secret_token = '44b99d40cc671854643922a7b2da97c54f70bf7473c852d56013fad20575aecf9f519bdcc376f166cca4bd39c3a00713c9dbbf577e4069023e36f1e21badf74e'
