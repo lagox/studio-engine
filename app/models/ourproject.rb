6119f98855074d1300ef7e23698455cc204dd070
lagox <lagox.mail@gmail.com>
2011-03-10 23:57:41 +0600

    add valid and sort

--- app/models/ourproject.rb
+++ app/models/ourproject.rb
@@ -1,3 +1,11 @@
+#encoding:utf-8
 class Ourproject < ActiveRecord::Base
-  validates :title, :url, :description, :presence => true
+  #url format
+  url_format = /^http:\/\/\w*/
+  
+  validates :title, :url, :description, :presence => {:message => " - поле не заполнено!"}
+  validates :url, :format => {:with => url_format, :message => " - Вы ввели url без http://"}
+  
+  #sort
+  default_scope order("id DESC")
 end
