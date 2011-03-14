d4a2ed99de533aac41981821b7a8131dd2d1fb9c
lagox <lagox.mail@gmail.com>
2011-03-15 02:46:20 +0600

    Finish projects

--- app/controllers/projects_controller.rb
+++ app/controllers/projects_controller.rb
@@ -11,6 +11,17 @@ class ProjectsController < ApplicationController
   end
 
   def show
+    begin
+      @project = Ourproject.find(params[:id])
+      @title = @project.title
+      respond_to do |format|
+        format.html # show.html.erb
+        format.xml  { render :xml => @project }
+      end
+    rescue ActiveRecord::RecordNotFound
+        flash[:notice] = "Такой записи не существует"
+        redirect_to(projects_path)
+    end
   end
 
 end
