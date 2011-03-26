9bf988448d3f758e4435f401c579f702c77490fa
lagox <lagox.mail@gmail.com>
2011-03-26 22:51:11 +0600

    edit cap

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,59 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+# _your_login_ - Поменять на ваш логин в панели управления
+# _your_project_ - Поменять на имя вашего проекта
+# _your_server_ - Поменять на имя вашего сервера (Указано на вкладке "FTP и SSH" вашей панели управления)
+# set :repository - Установить расположение вашего репозитория
+# У вас должна быть настроена авторизация ssh по сертификатам
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "remeli"
+set :repository,  "git://github.com/lagox/studio-engine.git"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+dpath = "/home/hosting_lagox/projects/remeli"
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+set :user, "hosting_lagox"
+set :rack_env, 'production'
+set :rails_env, 'production'
+set :use_sudo, false
+set :deploy_to, dpath
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+set :scm, :git
+
+role :web, "lithium.locum.ru"                          # Your HTTP server, Apache/etc
+role :app, "lithium.locum.ru"                          # This may be the same as your `Web` server
+role :db,  "lithium.locum.ru", :primary => true # This is where Rails migrations will run
+
+after "deploy:update_code", :copy_database_config
+
+task :copy_database_config, roles => :app do
+  db_config = "#{shared_path}/database.yml"
+  run "cp #{db_config} #{release_path}/config/database.yml"
+end
+
+#paperclip
+task :symlink_shared, roles => :app do
+  run "ln -nfs #{shared_path}/system #{release_path}/public/system"
+end
+after "deploy:update_code", :symlink_shared
+
+set :unicorn_rails, "/var/lib/gems/1.8/bin/unicorn_rails"
+set :unicorn_conf, "/etc/unicorn/remeli.lagox.rb"
+set :unicorn_pid, "/var/run/unicorn/remeli.lagox.pid"
+
+
+
+# - for unicorn - #
+namespace :deploy do
+  desc "Start application"
+  task :start, :roles => :app do
+    run "#{unicorn_rails} -Dc #{unicorn_conf}"
+  end
+
+  desc "Stop application"
+  task :stop, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
+  end
+
+  desc "Restart Application"
+  task :restart, :roles => :app do
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_rails} -Dc #{unicorn_conf}"
+  end
+end
